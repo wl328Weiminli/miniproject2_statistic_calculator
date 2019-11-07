f31fadb5a5aaed2c64fa5a63f962922cdfb7e137
dujiaxin <fashui01@163.com>
2019-11-07 15:41:28 -0500

    Feature: add absolutepath tool

--- CsvReader/CsvReader.py
+++ CsvReader/CsvReader.py
@@ -1,6 +1,6 @@
 import csv
 from pprint import pprint
-
+from Fileutilities.absolutepath import absolutepath
 
 def ClassFactory(class_name, dictionary):
     return type(class_name, (object,), dictionary)
@@ -12,7 +12,7 @@ class CsvReader:
     def __init__(self, filepath):
         self.data = []
         try:
-            with open(filepath) as text_data:
+            with open(absolutepath(filepath)) as text_data:
                 csv_data = csv.DictReader(text_data, delimiter=',')
                 for row in csv_data:
                     self.data.append(row)
