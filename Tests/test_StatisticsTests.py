3f67e9ca878ee81342ef2d97bdfa6475dda68df2
Weimin wl328 <wl328@njit.edu>
2019-11-10 14:50:06 -0500

    feature: run the test of mean; median; mode; standard deviation; variance.

--- Tests/test_StatisticsTests.py
+++ Tests/test_StatisticsTests.py
@@ -2,9 +2,16 @@ import unittest
 from CsvReader.CsvReader import CsvReader
 from Calculator.Calculator import Calculator
 from Statistics.Statistics import Statistics
+from pprint import pprint
 
 
 class MyTestCase(unittest.TestCase):
+    test_data = CsvReader('Tests/Data/Test_Data.csv').data
+    column = [int(row['data1']) for row in test_data]
+    column2 = [int(row['data2']) for row in test_data]
+    test_answer = CsvReader("Tests/Data/Test_dataAnswer.csv").data
+    pprint(column)
+
     def setUp(self) -> None:
         self.statistics = Statistics()
 
@@ -12,7 +19,34 @@ class MyTestCase(unittest.TestCase):
         self.assertIsInstance(self.statistics, Statistics)
 
     def test_mean_statistics(self):
-        pass
+        for row in self.test_answer:
+            pprint(row["average"])
+        self.assertEqual(self.statistics.population_mean(self.column), float(row['average']))
+        self.assertEqual(self.statistics.result, float(row['average']))
+
+    def test_median_statistic(self):
+        for row in self.test_answer:
+            pprint(row["median"])
+        self.assertEqual(self.statistics.median(self.column), float(row['median']))
+        self.assertEqual(self.statistics.result, float(row['median']))
+
+    def test_mode_statistic(self):
+        for row in self.test_answer:
+            pprint(row["mode"])
+        self.assertEqual(self.statistics.mode(self.column), [float(row['mode'])])
+        self.assertEqual(self.statistics.result, [float(row['mode'])])
+
+    def test_deviation_statistic(self):
+        for row in self.test_answer:
+            pprint(row["deviation"])
+        self.assertEqual(self.statistics.population_standard_deviation(self.column), float(row['deviation']))
+        self.assertEqual(self.statistics.result, float(row['deviation']))
+
+    def test_variance_statistic(self):
+        for row in self.test_answer:
+            pprint(row["variance"])
+        self.assertEqual(self.statistics.population_variance(self.column), float(row['variance']))
+        self.assertEqual(self.statistics.result, float(row['variance']))
 
 
 if __name__ == '__main__':
